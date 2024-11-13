83664bdb9af6d51eb57b0106744cd75cb6f86379
Moscvin <moscvin.2002@gmail.com>
2024-11-13 12:34:48 +0200

    first commit

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+
+
+
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        // public DbSet<Stock> Stocks { get; set; }
+        // public DbSet<Comment> Comments { get; set; } 
+    }
+}
